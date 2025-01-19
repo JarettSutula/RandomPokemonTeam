2c0396d908f3e509cc5992bdf59948ec955f25e4
JarettSutula <Jarett.Sutula1@marist.edu>
2025-01-19 11:34:31 -0500

    python concept working

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
 from utils import Pokemon
 from utils import Pokemon_Team
 from utils import fill_list
+from utils import grab_three_pokemon
+import time
 
+# start with filling the dex
 dex = fill_list()
+
+# Team Creation
+teams = []
+team_num = int(input("How many teams will there be? "))
+rerolls = int(input("How many rerolls will each team get? "))
+
+for i in range(team_num):
+    name = input("What is Player " + str(i+1) + "'s name? ")
+    new_team = Pokemon_Team(name, rerolls)
+    teams.append(new_team)
+
+# once teams are created, begin draft loop.
+for i in range(6):
+    # announce team, round, how many are left.
+    print(f'\n========= ROUND {i+1} =========')
+    print(f'There are {len(dex)} Pokemon remaining in the pool...')
+    filler = input()
+
+    for j in range(len(teams)):
+        # announce team
+        print(f'With Roll #{j+1} in Round {i+1}, {teams[j].name} rolls...')
+        picks, dex = grab_three_pokemon(teams[j].team, dex)
+        print(f'{picks[0].name}...')
+        # time.sleep(1)
+        print(f'{picks[1].name}...')
+        # time.sleep(1)
+        print(f'{picks[2].name}!')
+        teams[j].team.append(picks[2])
+        print()
+        # time.sleep(2)
+
+
+print(f'This concludes Team Creation. Results:')
+for team in teams:
+    team.print_team()
+    print()
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import random
+
 class Pokemon:
     def __init__(self, name, t1, t2):
         self.name = name
@@ -5,15 +7,16 @@ class Pokemon:
         self.t2 = t2
 
     def print_poke(self):
-        print(f'{self.name} - {self.t1} {self.t2}')
+        print(f'{self.name:10} - {self.t1:8} {self.t2:8}')
 
 class Pokemon_Team:
-    def __init__(self, name):
+    def __init__(self, name, rerolls):
         self.name = name
         self.team = []
+        self.rerolls = rerolls
 
     def print_team(self):
-        print(f'{self.name}''s Team:')
+        print(f"{self.name}'s Team:")
         for poke in self.team:
             poke.print_poke()
 
@@ -29,6 +32,41 @@ def fill_list():
 
     return dex_list
 
-my_dex = fill_list()
-for poke in my_dex:
-    poke.print_poke()
+def grab_three_pokemon(team, dex):
+    return_list = []
+    temps = []
+    # given a list, grab two 'fakes' for loot box style roll and one final pick.
+    fake_1 = random.choice(dex)
+    temps.append(fake_1)
+    return_list.append(fake_1)
+    dex.remove(fake_1)
+
+    fake_2 = random.choice(dex)
+    temps.append(fake_2)
+    dex.remove(fake_2)
+    return_list.append(fake_2)
+
+    can_pick_fire_type = True
+    for poke in team:
+        if poke.t1 == "fire":
+            can_pick_fire_type = False
+            print("Team already has a fire type.")
+
+    if not can_pick_fire_type:
+        for poke in dex:
+            if poke.t1 == "fire":
+                temps.append(poke)
+                print(f'removed {poke.name} from dex for this roll.') 
+                dex.remove(poke)    
+
+    pick = random.choice(dex)
+    dex.remove(pick)
+
+    return_list.append(pick)
+
+    # place fake picks back into dex!
+    for poke in temps:
+        dex.append(poke)
+    
+    # return picks and updated dex.
+    return return_list, dex
